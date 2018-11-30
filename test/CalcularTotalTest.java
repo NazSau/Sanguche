72ac4ed9f69c68f589de07b587b70ce5d342c778
NazSau <cnazarenosaurral@gmail.com>
2018-11-29 21:59:30 -0300

    Repositorio mil commit 2

--- test/CalcularTotalTest.java
+++ test/CalcularTotalTest.java
@@ -22,34 +22,23 @@ import sandw.dominio.Venta;
  */
 
 public class CalcularTotalTest {
-    @Test
+   
     public CalcularTotalTest() {
+    
+    }
+    
+    @Test
+    public void testCalcularTotal(){
     Producto p1=new Producto("Milanesa", 100, 100, null, 75);
-    Producto p2=new Producto("Lomito", 100, 101, null, 90);
-    Producto p3=new Producto("Mexicano", 100, 102, null, 150);
-    Producto p5=new Producto("Cerveza", 2, 105, null, 80.00);
     Producto p6=new Producto("Coca Cola", 50, 106, null, 40.00);
-    
-    
-    p1.addComponente(new Agregado("tomate", false));
-    p1.addComponente(new Agregado("lechuga", false));
-    p1.addComponente(new Agregado("mayonesa", false));
-    p1.addComponente(new Agregado("mostaza", false));
-    
-    
-    p2.addComponente(new Agregado("tomate", false));
-    p2.addComponente(new Agregado("lechuga", false));
-    p2.addComponente(new Agregado("mayonesa", false));
-    p2.addComponente(new Agregado("mostaza", false));
-    
     Venta venta = new Venta();
     venta.addProducto(p1);
     venta.addProducto(p6);
     
-    double res = venta.calcularTotal();
-    double esperado = 115.00;
+    double resultado = venta.calcularTotal();
+    double esperado = 115;
     
-    assertEquals(esperado, res);
+    assertTrue(resultado == esperado);
     }
     
 }
