a59eb96cc36c24524cd2b88b34ecbf4a213fecf9
NazSau <41141748+NazSau@users.noreply.github.com>
2018-12-07 19:38:39 -0300

    Add files via upload

--- test/CalcularTotalTest.java
+++ test/CalcularTotalTest.java
@@ -26,9 +26,22 @@ public class CalcularTotalTest {
     public CalcularTotalTest() {
     
     }
-    
+    //Calculo de Total con 1 producto
     @Test
-    public void testCalcularTotal(){
+    public void testCalcularTotal1(){
+    Producto p1=new Producto("Milanesa", 100, 100, null, 75);
+    Venta venta = new Venta();
+    venta.addProducto(p1);
+    
+    double resultado = venta.calcularTotal();
+    double esperado = 75;
+    
+    assertTrue(resultado == esperado);
+    }
+
+    //Calculo de Total con 2 productos
+    @Test
+    public void testCalcularTotal2(){
     Producto p1=new Producto("Milanesa", 100, 100, null, 75);
     Producto p6=new Producto("Coca Cola", 50, 106, null, 40.00);
     Venta venta = new Venta();
@@ -41,4 +54,51 @@ public class CalcularTotalTest {
     assertTrue(resultado == esperado);
     }
     
+//Calculo de Total con 1 producto con 1 agregado premium
+    @Test
+    public void testCalcularTotalAgregadoPremium(){
+    Producto p1=new Producto("Milanesa", 100, 100, null, 75);
+    Venta venta = new Venta();
+    p1.addPremiuns(new Agregado("Jamon", true,20));
+    venta.addProducto(p1);
+    
+    double resultado = venta.calcularTotal();
+    double esperado = 95;
+    
+    assertTrue(resultado == esperado);
+    }
+    
+    //Calculo de Total con 1 producto con 2 agregado premium
+    @Test
+    public void testCalcularTotal2AgregadosPremium(){
+    Producto p1=new Producto("Milanesa", 100, 100, null, 75);
+    Venta venta = new Venta();
+    p1.addPremiuns(new Agregado("Jamon", true,20));
+    p1.addPremiuns(new Agregado("Huevo", true,20));
+    venta.addProducto(p1);
+    
+    double resultado = venta.calcularTotal();
+    double esperado = 115;
+    
+    assertTrue(resultado == esperado);
+    }
+    
+    //Calculo de Total con 1 producto con 2 agregado premium
+    @Test
+    public void testCalcularTotal2Productos2AgregadosPremium(){
+    Producto p1=new Producto("Milanesa", 100, 100, null, 75);
+    Producto p6=new Producto("Coca Cola", 50, 106, null, 40.00);
+    Venta venta = new Venta();
+    p1.addPremiuns(new Agregado("Jamon", true,20));
+    p1.addPremiuns(new Agregado("Huevo", true,20));
+    p6.addPremiuns(new Agregado("Queso", true,30));
+    p6.addPremiuns(new Agregado("Huevo", true,20));
+    venta.addProducto(p1);
+    venta.addProducto(p6);
+    
+    double resultado = venta.calcularTotal();
+    double esperado = 205;
+    
+    assertTrue(resultado == esperado);
+    }
 }
